0bea6c131e39172fba421611f8cd61e3edb63236
Aniket Harshe <aniketharshe20@gmail.com>
2020-09-30 21:21:09 +0530

    Updates

---
+++ precache-manifest.00ce003ddc6f22213e9644a44484f36c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e726a49bc418f3ced8672c566722e28b",
+    "url": "/sprint-planner-react-bootstrap/index.html"
+  },
+  {
+    "revision": "59a10da7d1788a60a495",
+    "url": "/sprint-planner-react-bootstrap/static/css/main.5e209a6d.chunk.css"
+  },
+  {
+    "revision": "f18399a868ee53a6ce5c",
+    "url": "/sprint-planner-react-bootstrap/static/js/2.790bd964.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sprint-planner-react-bootstrap/static/js/2.790bd964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a10da7d1788a60a495",
+    "url": "/sprint-planner-react-bootstrap/static/js/main.0d3c395a.chunk.js"
+  },
+  {
+    "revision": "5dbab16d8c48f4b3c278",
+    "url": "/sprint-planner-react-bootstrap/static/js/runtime-main.7defe2d0.js"
+  }
+]);
